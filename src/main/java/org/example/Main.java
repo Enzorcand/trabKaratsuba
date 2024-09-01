5b6af05cb50029d733af4eeeeb1667c9b3d2743e
Enzorcand <enzorcand@gmail.com>
2024-09-01 12:38:25 -0300

    implementado a soma binaria

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,24 +4,14 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            String fator1 = "1000";
-            String fator2 = "11110";
-
-
-
+            String fator1 = "1100";
+            String fator2 = "11010";
+            System.out.println(somaBinaria(fator1, fator2));
     }
 
     private static String karatsuba(String fator1, String fator2) {
 
         //concatenação de zeros do menor numero para igualar os lenghts
-//        {
-//            int diff = fator1.length() - fator2.length();
-//            if (diff > 0) {
-//                fator2 = concatZeros(diff, fator2);
-//            } else {
-//                fator1 = concatZeros(-1 * diff, fator1);
-//            }
-//        }
 
         String a1 = fator1.substring(0, fator1.length()/2);
         String a2 = fator1.substring(fator1.length()/2);
@@ -42,9 +32,40 @@ public class Main {
     }
 
 
-    //TODO: Realiza uma soma simples binaria entre 2 strings de mesmo tamanho
     private static String somaBinaria(String b1, String b2) {
-        return null;
+        String result = "";
+        int carry = 0;
+        if (b1.length() != b2.length()) {
+            int diff = b1.length() - b2.length();
+            if (diff > 0) {
+                b2 = concatZeros(diff, b2);
+            } else {
+                b1 = concatZeros(-1 * diff, b1);
+            }
+        }
+        for (int i = b1.length() - 1; i >= 0; i--) {
+            int t = (int) b1.charAt(i) + (int) b2.charAt(i) + carry;
+            System.out.println(t);
+            switch (t) {
+                case 96:
+                    result = "0" + result;
+                    break;
+                case 97:
+                    result = "1" + result;
+                    break;
+                case 98:
+                    result = "0" + result;
+                    carry = 1;
+                    break;
+                case 99:
+                    result = "1" + result;
+                    carry = 1;
+            }
+        }
+        if (carry == 1) {
+            result = "1" + result;
+        }
+        return result;
     }
 
     private static String concatZeros(int n, String s) {
