147493c80ac291e0865d0de68345c467b669665c
Enzorcand <enzorcand@gmail.com>
2024-09-01 22:58:29 -0300

    LEMBRA QUE MULTIPLICA POR 2 E SOMA PELA MULTIPLICACAO POR 8

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,15 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            String fator1 = "1100";
-            String fator2 = "11010";
+            String fator1 = "11110";
+            String fator2 = "11100";
+            String fator3 = "1111000";
+
+
+            System.out.println(complemento2(fator2));
             System.out.println(somaBinaria(fator1, fator2));
+            System.out.println(subtraBinaria(fator1, fator2));
+            System.out.println(somaKaratsuba(fator1, fator3, fator2));
     }
 
     private static String karatsuba(String fator1, String fator2) {
@@ -28,7 +34,32 @@ public class Main {
     }
     //TODO: Realiza a seguinte operação em binario: (n1 << + (n2 - n1 - n3) < + n3
     private static String somaKaratsuba(String n1, String n2, String n3) {
-        return null;
+        n2 = subtraBinaria(n2, somaBinaria(n1, n3));
+        n1 = n1 + "00";
+        n2 = n2 + "0";
+
+        return somaBinaria(n1, somaBinaria(n2, n3));
+    }
+
+    private static String subtraBinaria(String n1, String n2) {
+        String result = "";
+        n2 = complemento2(n2);
+        result = somaBinaria(n1, n2);
+        result = result.replaceFirst("1", "");
+        return result;
+    }
+
+    private static String complemento2(String n2) {
+        String result = "";
+        for (char c : n2.toCharArray()) {
+            if(c == '1'){
+                result = result + '0';
+            } else {
+                result = result + '1';
+            }
+        }
+        result = somaBinaria(result, "1");
+        return result;
     }
 
 
@@ -45,13 +76,14 @@ public class Main {
         }
         for (int i = b1.length() - 1; i >= 0; i--) {
             int t = (int) b1.charAt(i) + (int) b2.charAt(i) + carry;
-            System.out.println(t);
             switch (t) {
                 case 96:
                     result = "0" + result;
+                    carry = 0;
                     break;
                 case 97:
                     result = "1" + result;
+                    carry = 0;
                     break;
                 case 98:
                     result = "0" + result;
