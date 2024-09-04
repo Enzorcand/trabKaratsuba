84eaab1bb2c8fb5305644e8894c0e4f715ecb0e4
enzorcand <enzorcand@gmail.com>
2024-09-04 19:43:30 -0300

    programa concluido e funcional

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,39 +4,52 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            String fator1 = "11100";
-            String fator2 = "11110";
-            String fator3 = "1111000";
+            String fator1 = args[0];
+            String fator2 = args[1];
 
-
-            System.out.println(complemento2(fator2));
-            System.out.println(somaBinaria(fator1, fator2));
-            System.out.println(subtraBinaria(fator1, fator2));
-            System.out.println(somaKaratsuba(fator1, fator3, fator2));
+            System.out.println(karatsuba(fator1, fator2));
     }
 
     private static String karatsuba(String fator1, String fator2) {
 
-        //concatenação de zeros do menor numero para igualar os lenghts
+        fator1 = fator1.replaceFirst("^0+(?!$)", "");
+        fator2 = fator2.replaceFirst("^0+(?!$)", "");
 
-        String a1 = fator1.substring(0, fator1.length()/2);
-        String a2 = fator1.substring(fator1.length()/2);
+        int diff = fator1.length() - fator2.length();
 
-        String b1 = fator2.substring(0, fator2.length()/2);
-        String b2 = fator2.substring(fator2.length()/2);
+        if (diff < 0){
+            fator1 = concatZeros(-diff,fator1);
+        } else {
+            fator2 = concatZeros(diff, fator2);
+        }
+
+        if (fator1.length()*fator2.length() == 1) {
+            return Integer.toString((fator1.charAt(0) - '0') * (fator2.charAt(0) - '0'));
+        }
+
+        int tamanho = fator1.length()/2;
+
+        String a1 = fator1.substring(0, fator1.length() - tamanho);
+        String a2 = fator1.substring(fator1.length() - tamanho);
+
+        String b1 = fator2.substring(0, fator2.length() - tamanho);
+        String b2 = fator2.substring( fator2.length() - tamanho);
 
         String n1 = karatsuba(a1, b1);
         String n2 = karatsuba(somaBinaria(a1,a2), somaBinaria(b1,b2));
         String n3 = karatsuba(a2, b2);
 
-        return somaKaratsuba(n1, n2, n3);
+        return somaKaratsuba(n1, n2, n3, tamanho).replaceFirst("^0+(?!$)", "");
     }
-    //TODO: Realiza a seguinte operação em binario: (n1 << + (n2 - n1 - n3) < + n3
-    private static String somaKaratsuba(String n1, String n2, String n3) {
+
+    private static String somaKaratsuba(String n1, String n2, String n3, int tamanho) {
         n2 = subtraBinaria(n2, somaBinaria(n1, n3));
-        n1 = somaBinaria(n1 + "0", n1 + "000");
-        n1 = somaBinaria(n1 + "0", n1 + "000");
-        n2 = somaBinaria(n2 + "0", n2 + "000");
+        for (int i = 0; i < tamanho*2; i++) {
+            n1 = n1 + '0';
+        }
+        for (int i = 0; i < tamanho; i++) {
+            n2 = n2 + '0';
+        }
         return somaBinaria(n1, somaBinaria(n2, n3));
     }
 
@@ -75,23 +88,25 @@ public class Main {
         }
         for (int i = b1.length() - 1; i >= 0; i--) {
             int t = (int) b1.charAt(i) + (int) b2.charAt(i) + carry;
-            switch (t) {
-                case 96:
+            carry = switch (t) {
+                case 96 -> {
                     result = "0" + result;
-                    carry = 0;
-                    break;
-                case 97:
+                    yield 0;
+                }
+                case 97 -> {
                     result = "1" + result;
-                    carry = 0;
-                    break;
-                case 98:
+                    yield 0;
+                }
+                case 98 -> {
                     result = "0" + result;
-                    carry = 1;
-                    break;
-                case 99:
+                    yield 1;
+                }
+                case 99 -> {
                     result = "1" + result;
-                    carry = 1;
-            }
+                    yield 1;
+                }
+                default -> carry;
+            };
         }
         if (carry == 1) {
             result = "1" + result;
